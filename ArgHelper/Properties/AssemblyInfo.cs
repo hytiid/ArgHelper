7ec05885072648a251631fa6959b2cc7f3894a1f
hytiid <hytiid@gmail.com>
2020-07-25 16:49:09 +0900

    modify: update .gitignore to ignore nuget related files. update assembly info.

--- ArgHelper/Properties/AssemblyInfo.cs
+++ ArgHelper/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ArgHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("arg helper for c# console application")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("hytiid")]
 [assembly: AssemblyProduct("ArgHelper")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
